1191463ae1b8faa699cce1f4986b2c30d508afd9
Hubert Yu <31826788+yyu18@users.noreply.github.com>
2020-08-24 10:03:29 -0400

    Create 1-profileServer.js

---
+++ 1-profileServer.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+var express = require('express');
+var app = express();
+const path = require("path");
+//var https = require("https");
+const allowCrossDomain = require('./utils/allowCrossDomain');
+const { errorHandler } = require('./utils/error');
+
+const myProfileRouter = require('./myProfile/my-profile-router.js');
+
+app.use(express.json());
+app.use(allowCrossDomain)
+//begin https with credential key
+         /*var options = {
+            key: fs.readFileSync('/etc/nginx/ssl/wildcard.singtao.ca/singtao.ca.key'),
+            cert: fs.readFileSync('/etc/nginx/ssl/wildcard.singtao.ca/combine.crt')
+           // ca: fs.readFileSync('/etc/ssl/wildcard.singtao.ca/gd_bundle-g2-g1.crt'), 
+        }
+        https.createServer(options,app).listen(5000,function(){
+            console.log('listening on 5000')
+        });*/
+ //end https with credential key
+//app listen to the external ip
+//app.listen(5000, "0.0.0.0",function() { console.log('Example app listening on port 5000!'); });
+//end app listen to the external ip
+
+app.listen(5000,'0.0.0.0',function() { 
+    console.log('Example app listening on port 5000!');});
+
+app.response.sendStatus = function (statusCode, type, message) {
+    return this.contentType(type)
+      .status(statusCode)
+      .send(message)
+}
+
+app.use('/static/image-profile', express.static(path.join(__dirname, 'uploads')))
+app.use('/profiles',myProfileRouter);
+app.use(errorHandler);
